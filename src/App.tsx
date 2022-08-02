80504cbd0e9c9f18ccd02b18321f701bacfc9baa
Alexandre Henrique Landgraf <alexandrehlandgraf@gmail.com>
2022-08-02 19:44:50 -0300

    adicionando material

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
+import {
+  Container,
+  createTheme,
+  CssBaseline,
+  Grid,
+  ThemeProvider,
+} from '@mui/material';
+import Navbar from './Navbar';
+import RecipeCard from './RecipeCard';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <h1>app</h1>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Navbar />
+      <Container sx={{ pt: 4 }}>
+        <Grid container spacing={4}>
+          {[1, 2, 3, 4, 5].map((i) => (
+            <Grid item>
+              <RecipeCard />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </ThemeProvider>
   );
 }
 
