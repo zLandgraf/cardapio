e02dc386953d800b9d97010b545eed27e68761e0
Alexandre Henrique Landgraf <alexandrehlandgraf@gmail.com>
2022-08-11 18:32:15 -0300

    adding ingredients list

--- src/menu/page.tsx
+++ src/menu/page.tsx
@@ -1,6 +1,17 @@
 import {
-  Grid, Paper, Typography,
+  Grid,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  Paper,
+  Typography,
+  Checkbox,
+  ListItemText,
+  Divider,
+  ListItemSecondaryAction,
 } from '@mui/material';
+import PrintIcon from '@mui/icons-material/Print';
 
 const MenuPage = () => (
   <Paper>
@@ -11,9 +22,32 @@ const MenuPage = () => (
         </Typography>
       </Grid>
       <Grid item xs={4} sx={{ backgroundColor: 'rgba(255, 255, 255, 0.05)', p: 2 }}>
-        <Typography variant="h6">
-          Ingredientes
-        </Typography>
+        <Grid container justifyContent="space-between" alignItems="center" mb={2}>
+          <Typography variant="h6">
+            Lista
+          </Typography>
+          <IconButton>
+            <PrintIcon />
+          </IconButton>
+        </Grid>
+        <Grid container>
+          <List sx={{ width: '100%' }} disablePadding>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((n) => (
+              <>
+                <ListItem key={n}>
+                  <ListItemIcon>
+                    <Checkbox />
+                  </ListItemIcon>
+                  <ListItemText primary="Ingrediente" />
+                  <ListItemSecondaryAction>
+                    {n}
+                  </ListItemSecondaryAction>
+                </ListItem>
+                <Divider variant="inset" component="li" />
+              </>
+            ))}
+          </List>
+        </Grid>
       </Grid>
     </Grid>
   </Paper>
